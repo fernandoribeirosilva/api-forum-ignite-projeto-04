d5767afd465c168f3f8dfc8dca761b708e81b9f9
fernandoribeirosilva <fernando1996ribeirosilva@gmail.com>
2023-06-13 06:00:23 -0300

    refactor: foi retirada a pasta core da pasta domain

---
+++ src/core/entities/entity.ts
@@ -0,0 +1,15 @@
+import { UniqueEntityID } from "./unique-entity-id"
+
+export class Entity<props> {
+  private _id: UniqueEntityID
+  protected props: props
+
+  get id() {
+    return this._id
+  }
+
+  protected constructor(props: props, id?: UniqueEntityID) {
+    this._id = id ?? new UniqueEntityID()
+    this.props = props
+  }
+}
