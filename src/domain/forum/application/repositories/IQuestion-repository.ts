73294b82f5d7c44ff11c8970c59b40895fd59ee5
fernandoribeirosilva <fernando1996ribeirosilva@gmail.com>
2023-06-14 05:28:30 -0300

    feat: criado método de buscar question pelo slug

--- src/domain/forum/application/repositories/IQuestion-repository.ts
+++ src/domain/forum/application/repositories/IQuestion-repository.ts
@@ -1,5 +1,6 @@
 import { Question } from '../../enterprise/entities/question'
 
 export interface IQuestionRepository {
+  findBySlug(slug: string): Promise<Question | null>
   create(question: Question): Promise<void>
 }
