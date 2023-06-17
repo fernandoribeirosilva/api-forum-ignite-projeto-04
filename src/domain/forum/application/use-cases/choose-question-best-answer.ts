92b5a1594db40fa5963004446f8615d9958d37de
fernandoribeirosilva <fernando1996ribeirosilva@gmail.com>
2023-06-17 07:53:50 -0300

    feat: criado o use case para marcar a melhor resposta de uma pergunta, onde somente o autor da pergunta pode marcar. Além disso, é implementado o respectivo teste unitário para garantir o correto funcionamento do caso de uso.

---
+++ src/domain/forum/application/use-cases/choose-question-best-answer.ts
@@ -0,0 +1,48 @@
+import { Question } from '../../enterprise/entities/question'
+import { IAnswerRepository } from '../repositories/IAnswers-repository'
+import { IQuestionRepository } from '../repositories/IQuestion-repository'
+
+interface ChooseQuestionBestAnswerUseCaseRequest {
+  authorId: string
+  answerId: string
+}
+interface ChooseQuestionBestAnswerUseCaseResponse {
+  question: Question
+}
+
+export class ChooseQuestionBestAnswerUseCase {
+  constructor(
+    private questionRepository: IQuestionRepository,
+    private answerRepository: IAnswerRepository,
+  ) {}
+
+  async execute({
+    authorId,
+    answerId,
+  }: ChooseQuestionBestAnswerUseCaseRequest): Promise<ChooseQuestionBestAnswerUseCaseResponse> {
+    const answer = await this.answerRepository.findById(answerId)
+
+    if (!answer) {
+      throw new Error('Answer not found.')
+    }
+
+    const question = await this.questionRepository.findById(
+      answer.questionId.toString(),
+    )
+
+    if (!question) {
+      throw new Error('Question not found.')
+    }
+
+    if (authorId !== question.authorId.toString()) {
+      throw new Error('Not allowed.')
+    }
+
+    question.bestAnswerId = answer.id
+    await this.questionRepository.save(question)
+
+    return {
+      question,
+    }
+  }
+}
